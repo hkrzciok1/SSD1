94470ec2fb7f873cdb405a08d26e6d8e8feda66e
hkrzciok1 <101844517+hkrzciok1@users.noreply.github.com>
2022-10-05 09:30:58 -0700

    Update testpoint.py

--- testpoint.py
+++ testpoint.py
@@ -2,8 +2,8 @@ def testpoint():
     xcor = 3
     ycor = 5
     test = Point()
-    test.set(xcor)
-    test.set(ycor)
+    test.setX(xcor)
+    test.setY(ycor)
 
     assert test.getx() == xcor
     assert test.gety() == ycor

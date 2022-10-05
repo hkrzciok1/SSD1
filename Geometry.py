f678ddde7d516522c4aab639bc4be18a1e25e908
cdd7711 <113385530+cdd7711@users.noreply.github.com>
2022-10-05 13:41:24 -0400

    Added geometry file (point class)

---
+++ Geometry.py
@@ -0,0 +1,16 @@
+class Point:
+	def __init__(self):
+		self.xcor = None
+		self.ycor = None
+
+	def setX(self, X):
+		self.xcor = X
+
+	def setY(self, Y):
+		self.ycor = Y
+
+	def getX(self):
+		return self.xcor
+
+	def getY(self):
+		return self.ycor

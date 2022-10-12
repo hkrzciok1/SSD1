30b26097320b3fa22e5b25387e64345d433a6ff2
hkrzciok1 <101844517+hkrzciok1@users.noreply.github.com>
2022-10-12 10:09:36 -0700

    Update testdistance.py

--- testdistance.py
+++ testdistance.py
@@ -9,4 +9,17 @@ def testdistance():
 	
 	assert Distance(point1, point2) == 5
 	
+	point1.setX(2)
+	point1.setY(-3)
+	point2.setX(-4)
+	point2.setY(0)
 	
+	assert Distance(point1, point2) == 3.61
+	# we are planning on using 2 places after decimal in our distance function
+	
+	point1.setX(2.5)
+	point1.setY(-3)
+	point2.setX(-4)
+	point2.setY(6.8)
+
+	assert Distance(point1, point2) == 4.09
